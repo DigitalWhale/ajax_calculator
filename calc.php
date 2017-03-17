60fce17b285e4e9c76d6460574d9744f040c9d79
DigitalWhale <jargiv@ya.ru>
2017-03-17 03:26:27 +0300

    Create parse sqrt and  trigonometric

--- calc.php
+++ calc.php
@@ -5,6 +5,7 @@ $FOR_REG_ONE_OP = '\^';
 $FOR_REG_TWO_OP = '(\*|\/)';
 $FOR_REG_THREE_OP = '(\+|-)';
 $REG_BRACKETS = '/\(.*\)/';
+$REG_FUN = '/((sin)|(cos)|(tan)|(ctan)|(sqrt))\(.*\)/';
 
 function math($mat, $OP){
     global $FOR_REG_NUM;
@@ -26,6 +27,44 @@ function math($mat, $OP){
             return $dig[0]-$dig[1];
             break;
     }
+}
+
+function mathFun($func, $dig){
+    switch($func){
+        case 'sqrt':
+            return sqrt($dig);
+        case 'cos':
+            return cos(deg2rad($dig));
+            break;
+        case 'sin':
+            return sin(deg2rad($dig));
+            break;
+        case 'tan':
+            return tan(deg2rad($dig));
+            break;
+        case 'ctan':
+            return 1/tan(deg2rad($dig));
+            break;
+    }
+}
+
+function findFunc($req){
+    global $REG_FUN;
+    $count = 0;
+    $req = preg_replace_callback($REG_FUN, function($mat){
+        $req = findOneOp($mat[0]);
+        $req = findTwoOp($req);
+        $req = findThreeOp($req);
+        $dig = preg_replace('/[a-z]|\(|\)/', '', $req);
+        $fun = preg_replace('/\d*|\(|\)/', '', $req);
+        $req = mathFun($fun, $dig);
+        return $req;
+    }, $req, -1, $count);
+    if($count === 0){
+        return $req;
+    }
+    $req = findFunc($req);
+    return $req;
 }
 
 function findBrackets($req){
@@ -98,6 +137,7 @@ if(!empty($req)){
 //        return "LOL";
 //    }, $req, -1, $count);
 
+    $req = findFunc($req);
     $req = findBrackets($req);
     $req = findOneOp($req);
     $req = findTwoOp($req);
