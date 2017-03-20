1eee2d485bee49af073546fe0b2eae429a340d19
DigitalWhale <jargiv@ya.ru>
2017-03-20 20:25:58 +0300

    Refactoring

--- calc.php
+++ calc.php
@@ -1,11 +1,6 @@
 <?php
 
 $FOR_REG_NUM = '-?\d+\.?\d*';
-$FOR_REG_ONE_OP = '\^';
-$FOR_REG_TWO_OP = '(\*|\/)';
-$FOR_REG_THREE_OP = '(\+|-)';
-$REG_BRACKETS = '/\([0-9\+\-\*\/\,\.]*\)/';
-$REG_FUN = '/((sin)|(cos)|(tg)|(ctg)|(sqrt))\([0-9\+\-\*\/\,\.]*\)/';
 
 function normalizeDeg($deg){
     if($deg > 0){
@@ -21,11 +16,10 @@ function normalizeDeg($deg){
     return $deg;
 }
 
-function math($mat, $OP){
+function math($mat, $op){
     global $FOR_REG_NUM;
-    $op = array_values(preg_grep('/'.$OP.'/', str_split($mat)));
-    $dig = array_values(preg_grep('/'.$FOR_REG_NUM.'/', explode($op[0], $mat)));
-    switch($op[0]){
+    $dig = array_values(preg_grep('/'.$FOR_REG_NUM.'/', explode($op, $mat)));
+    switch($op){
         case '^':
             $res =  pow($dig[0],$dig[1]);
             break;
@@ -82,12 +76,16 @@ function mathFun($func, $dig){
     return round($res, 5);
 }
 
-function findFunc($req){
-    global $REG_FUN;
+function findAr($mat){
+    $req = findMath($mat, '-one');
+    $req = findMath($req, '-two');
+    $req = findMath($req, '-three');
+    return $req;
+}
+
+function findFunc($req, $REG_FUN){
     $req = preg_replace_callback($REG_FUN, function($mat){
-        $req = findOp($mat[0], '-one');
-        $req = findOp($req, '-two');
-        $req = findOp($req, '-three');
+        $req = findAr($mat[0]);
         $dig = preg_replace('/[a-z]|\(|\)/', '', $req);
         $fun = preg_replace('/\.|,|\d*|\(|\)/', '', $req);
         $req = mathFun($fun, $dig);
@@ -96,82 +94,64 @@ function findFunc($req){
     return [$req, $count];
 }
 
-function findBrackets($req){
-    global $REG_BRACKETS;
-    $count = 0;
+function findBrackets($req, $REG_BRACKETS){
     $req = preg_replace_callback($REG_BRACKETS, function($mat){
-        $req = findOp($mat[0], '-one');
-        $req = findOp($req, '-two');
-        $req = findOp($req, '-three');
+        $req = findAr($mat[0]);
         $req = preg_replace('/\(|\)/', '', $req);
         return $req;
     }, $req, -1, $count);
     return [$req, $count];
 }
 
-function findOneOp($req){
+function findOp($req, $REG_OP){
     global $FOR_REG_NUM;
-    global $FOR_REG_ONE_OP;
-    $req = preg_replace_callback('/'.$FOR_REG_NUM.$FOR_REG_ONE_OP.$FOR_REG_NUM.'/', function($mat){
-        global $FOR_REG_ONE_OP;
-        return math($mat[0], $FOR_REG_ONE_OP );
+    $req = preg_replace_callback('/'.$FOR_REG_NUM.$REG_OP.$FOR_REG_NUM.'/', function($mat){
+        global $FOR_REG_NUM;
+        $REG_OP  = array_values(preg_grep('/'.$FOR_REG_NUM.'/', str_split($mat[0]), PREG_GREP_INVERT));
+        return math($mat[0], $REG_OP[0] );
     }, $req, -1, $count);
     return [$req, $count];
 }
 
-function findTwoOp($req){
-    global $FOR_REG_NUM;
-    global $FOR_REG_TWO_OP;
-    $req = preg_replace_callback('/'.$FOR_REG_NUM.$FOR_REG_TWO_OP.$FOR_REG_NUM.'/', function($mat){
-        global $FOR_REG_TWO_OP;
-        return math($mat[0], $FOR_REG_TWO_OP );
-    }, $req, -1, $count);
-    return [$req, $count];
-}
-
-function findThreeOp($req){
-    global $FOR_REG_NUM;
-    global $FOR_REG_THREE_OP;
-    $req = preg_replace_callback('/'.$FOR_REG_NUM.$FOR_REG_THREE_OP.$FOR_REG_NUM.'/', function($mat){
-        global $FOR_REG_THREE_OP;
-        return math($mat[0], $FOR_REG_THREE_OP );
-    }, $req, -1, $count);
-    return [$req, $count];
-}
-
-function findOp($req, $op){
+function findMath($req, $op){
+    $FOR_REG_ONE_OP = '\^';
+    $FOR_REG_TWO_OP = '(\*|\/)';
+    $FOR_REG_THREE_OP = '(\+|-)';
+    $REG_BRACKETS = '/\([0-9\+\-\*\/\,\.]*\)/';
+    $REG_FUN = '/((sin)|(cos)|(tg)|(ctg)|(sqrt))\([0-9\+\-\*\/\,\.]*\)/';
     switch($op){
         case '-one':
-            $res = findOneOp($req);
+            $res = findOp($req, $FOR_REG_ONE_OP);
             break;
         case '-two':
-            $res = findTwoOp($req);
+            $res = findOp($req, $FOR_REG_TWO_OP);
             break;
         case '-three':
-            $res = findThreeOp($req);
+            $res = findOp($req, $FOR_REG_THREE_OP);
             break;
         case '-br':
-            $res = findBrackets($req);
+            $res = findBrackets($req, $REG_BRACKETS);
             break;
         case '-f':
-            $res = findFunc($req);
+            $res = findFunc($req, $REG_FUN);
             break;
     }
     if($res[1] === 0){
         return $res[0];
     }
-    $res[0] = findOp($res[0], $op);
+    $res[0] = findMath($res[0], $op);
     return $res[0];
 }
 
+function parseExpression($exp){
+    if(!empty($exp)){
+        $exp = preg_replace('/\s/', '', $exp);
+        $exp = findMath($exp, '-f');
+        $exp = findMath($exp, '-br');
+        $exp = $req = findAr($exp);
+        echo $exp;
+    }
+}
 
 $req = $_POST["math"];
-if(!empty($req)){
-    $req = preg_replace('/\s/', '', $req);
-    $req = findOp($req, '-f');
-    $req = findOp($req, '-br');
-    $req = findOp($req, '-one');
-    $req = findOp($req, '-two');
-    $req = findOp($req, '-three');
-    echo $req;
-}
+parseExpression($req);
